01bcac1d63fa5e731d1756baf2df17ad2b6d63d0
syzygy007 <dgermann@main.inc>
2026-09-23 13:51:49 -0700

    Delete netlify directory

--- netlify/functions/me.js
+++
@@ -1,8 +0,0 @@
-// POST -> {ok, owner} when the session cookie is valid
-const S = require('./_shared');
-
-exports.handler = async (event) => {
-    const owner = await S.readSession(event);
-    if (!owner) return S.json(401, { ok: false });
-    return S.json(200, { ok: true, owner });
-};
--- netlify/functions/signout.js
+++
@@ -1,7 +0,0 @@
-// POST -> clears the session cookie (and deletes the server side session)
-const S = require('./_shared');
-
-exports.handler = async (event) => {
-    await S.destroySession(event);
-    return S.json(200, { ok: true }, { 'Set-Cookie': S.clearSessionCookie() });
-};
